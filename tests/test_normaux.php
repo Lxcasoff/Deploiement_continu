2ceb0645a969594a09e31ce987b645010799dfe5
Tomm <tom.picout@nopestudio.fr>
2023-04-07 16:37:29 +0200

    🐛 FIX: test

--- tests/test_normaux.php
+++ tests/test_normaux.php
@@ -1,15 +1,15 @@
 <?php
     include("../new.php");
-    $ret5 = create_user("username$", "password");
-    $ret7 = create_user("username", "passwor");
-    $ret12 = create_user("USERNAME", "password");
-    $ret15 = create_user("USERNAME", "password!.");
-    if (($ret5 == 2) && ($ret7 == 3) && ($ret12 == 5) && ($ret15 == 0)) {
+    $ret1 = create_user("username$", "password");
+    $ret2 = create_user("username", "passwor");
+    $ret3 = create_user("USERNAME", "password");
+    $ret4 = create_user("USERNAME", "password!.");
+    if (($ret1 == 2) && ($ret2 == 3) && ($ret3 == 5) && ($ret4 == 0)) {
         printf("true");
     }else {
         printf("false");
-        printf($ret5);
-        printf($ret7);
-        printf($ret12);
-        printf($ret15);
+        printf($ret1);
+        printf($ret2);
+        printf($ret3);
+        printf($ret4);
     }
