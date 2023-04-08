3a27b795636244d3651fce33a51225a334b30eef
LCS <99267496+Lxcasoff@users.noreply.github.com>
2023-04-08 10:15:47 +0200

    Update disconnect.php

--- disconnect.php
+++ disconnect.php
@@ -1,7 +1,7 @@
 <?php
   //session_start();
     function disconnect_user() {
-        session_start();
+        //session_start();
         if (isset($_SESSION["CONNECTED"])) {
             unset($_SESSION["CONNECTED"]);
         }
